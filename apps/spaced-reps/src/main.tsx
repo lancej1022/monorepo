a5c33120857a2a48f7382db270beeb4e2728e052
lancej1022 <46359098+lancej1022@users.noreply.github.com>
2024-09-21 20:11:18 -0700

    feat(spaced-reps): scaffold base tanstack router app

---
+++ apps/spaced-reps/src/main.tsx
@@ -0,0 +1,28 @@
+import { createRouter, RouterProvider } from '@tanstack/react-router'
+import ReactDOM from 'react-dom/client'
+
+import { routeTree } from './routeTree.gen'
+
+// Set up a Router instance
+const router = createRouter({
+	routeTree,
+	defaultPreload: 'intent',
+})
+
+// Register things for typesafety
+declare module '@tanstack/react-router' {
+	interface Register {
+		router: typeof router
+	}
+}
+
+const rootElement = document.getElementById('app')
+if (!rootElement)
+	throw Error(
+		'Could not locate root `#app` id on any element. Unable to mount react tree, throwing Error.',
+	)
+
+if (!rootElement.innerHTML) {
+	const root = ReactDOM.createRoot(rootElement)
+	root.render(<RouterProvider router={router} />)
+}
