b240c3c2345e4e52e07d14ab5d2ab3b01dbf9a6b
lancej1022 <46359098+lancej1022@users.noreply.github.com>
2024-10-16 15:08:01 -0700

    feat(spaced-reps): add support for neetcode reminders
    Refactor the `parseUrl` function to support neetcode, leetcode, and greatfrontend reminders.

--- apps/spaced-reps/src/utils/parseUrl.ts
+++ apps/spaced-reps/src/utils/parseUrl.ts
@@ -1,22 +1,31 @@
+/** contains the index to use in `pathParts` array to determine the problem name  */
+const nameToIndexMap = {
+	'greatfrontend.com': -1,
+	'neetcode.io': -1,
+	'leetcode.com': 2,
+} as const
+
+type Hostname = keyof typeof nameToIndexMap
+
+function isValidDomain(hostname: string): hostname is Hostname {
+	return hostname in nameToIndexMap
+}
+
 export function parseUrl(url: string) {
 	const urlObj = new URL(url)
 	const { pathname, hostname } = urlObj
 
-	// TODO: might be able to DRY this up by abstracting around the `.at(index)`
-	// but need to add neetcode case as well before deciding if theres actually a viable abstraction here
-	switch (true) {
-		case hostname.includes('greatfrontend'): {
-			const pathParts = pathname.split('/')
-			const problemName = pathParts.at(-1)
-			return problemName
-		}
-		case hostname.includes('leetcode'): {
-			const pathParts = pathname.split('/')
-			const problemName = pathParts[2]
-			return problemName
-		}
-		default: {
-			throw Error(`unsupported hostname: ${hostname}`)
-		}
+	const hostParts = hostname.split('www.')
+	const domainName = (hostParts.length > 1 ? hostParts.at(1) : hostParts.at(0)) ?? ''
+
+	if (!isValidDomain(domainName)) {
+		throw Error(`unsupported hostname: ${domainName}`)
 	}
+
+	const pathParts = pathname.split('/')
+	// eslint-disable-next-line security/detect-object-injection -- TODO: Review this security error. It should theoretically be safe thanks to `isValidDomain()` check above
+	const indexForProblemName = nameToIndexMap[domainName]
+	const problemName = pathParts.at(indexForProblemName)
+
+	return problemName
 }
